896da3b4f206fede422fe44b898d7e741b890f25
Kieran Boyce <Kieran2.Boyce@live.uwe.ac.uk>
2020-10-26 14:51:02 +0000

    Added Boss wall
    Wall blocking the player appears when the player enters the boss room

---
+++ DMAKE-PROJECT/Assets/Scripts/Activateboss.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activateboss : MonoBehaviour
+{
+
+    public GameObject hedge, hedge1, hedge2;
+    bool bossstart = false;
+    public GameObject player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Collider2D playercollider = player.GetComponent<Collider2D>();
+        if (collision == playercollider)
+        {
+            hedge.active = true;
+            hedge1.active = true;
+            hedge2.active = true;
+            Debug.Log("Commence");
+        }
+    }
+   
+}
